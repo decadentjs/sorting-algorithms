a16f04c7b9617ba849cd88679a88727d94c4fa21
claudiopro <claudio.procida@gmail.com>
2015-11-28 18:43:28 +0000

    Semicolons and .jshintrc

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-var React = require('react')
-var ReactDOM = require('react-dom')
-var ud = require('ud')
-import App from './lib/app.js'
+var React = require('react');
+var ReactDOM = require('react-dom');
+var ud = require('ud');
+import App from './lib/app.js';
 
 ReactDOM.render(
   React.createElement(ud.defn(module, App), null),
   document.querySelector('#content')
-)
+);
