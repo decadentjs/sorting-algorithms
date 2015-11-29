519b150f2b71e40db2a3872bec08535b2ebce263
claudiopro <claudio.procida@gmail.com>
2015-11-29 12:24:01 +0000

    Sorter sorts with selection sort

--- src/app.js
+++ src/app.js
@@ -1,5 +1,35 @@
 var React = require('react');
 
+function selection_sort(a) {
+  /* a[0] to a[n-1] is the array to sort */
+  var i, j, n = a.length;
+  var iMin, tmp;
+
+  /* advance the position through the entire array */
+  /*   (could do j < n-1 because single element is also min element) */
+  for (j = 0; j < n-1; j++) {
+    /* find the min element in the unsorted a[j .. n-1] */
+
+    /* assume the min is the first element */
+    iMin = j;
+    /* test against elements after j to find the smallest */
+    for ( i = j+1; i < n; i++) {
+      /* if this element is less, then it is the new minimum */
+      if (a[i] < a[iMin]) {
+        /* found new minimum; remember its index */
+        iMin = i;
+      }
+    }
+
+    if (iMin != j) {
+      tmp = a[j];
+      a[j] = a[iMin];
+      a[iMin] = tmp;
+      return;
+    }
+  }
+}
+
 class Sorter extends React.Component {
   constructor (props) {
     super(props);
@@ -12,13 +42,8 @@ class Sorter extends React.Component {
   componentDidMount () {
     console.log('mounted');
     setInterval(function() {
-      var list = this.state.list;
-      var i = Math.floor(list.length * Math.random());
-      var j = Math.floor(list.length * Math.random());
-      var tmp = list[i];
-      list[i] = list[j];
-      list[j] = tmp;
-      this.setState({list: list});
+      selection_sort(this.state.list);
+      this.setState({list: this.state.list});
     }.bind(this), 1000);
   }
   render () {
