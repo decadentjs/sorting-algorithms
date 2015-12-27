3fc04a549c29d6bfbf90708a29846370031babc2
claudiopro <claudio.procida@gmail.com>
2015-12-28 00:51:17 +0100

    Adds nearly sorted, reversed and few uniques

--- src/app.js
+++ src/app.js
@@ -1,21 +1,97 @@
 var React = require('react');
 import Sorter from './sorter.js';
 
+function swap(arr, a, b) {
+  var t;
+  t = arr[a];
+  arr[a] = arr[b];
+  arr[b] = t;
+}
+
 class App extends React.Component {
   constructor (props) {
     super(props);
+    this.state = this.getState();
+  }
+
+  getState () {
+    var i, random_buf = [];
+    for (i = 0; i < 20; i++) {
+      random_buf.push(250 * Math.random());
+    }
+
+    var partially_ordered_buf = [];
+    for (i = 0; i < 20; i++) {
+      partially_ordered_buf.push(250 / 20 * (i + 1));
+    }
+    swap(partially_ordered_buf, 2, 4);
+    swap(partially_ordered_buf, 2, 3);
+    swap(partially_ordered_buf, 7, 8);
+    swap(partially_ordered_buf, 11, 12);
+    swap(partially_ordered_buf, 13, 15);
+    swap(partially_ordered_buf, 13, 14);
+    swap(partially_ordered_buf, 16, 18);
+
+    var reversed_buf = [];
+    for (i = 0; i < 20; i++) {
+      reversed_buf.push(250 / 20 * (20 - i));
+    }
+
+    var few_unique_buf = [];
+    for (i = 0; i < 20; i++) {
+      few_unique_buf.push(250 * Math.ceil(4 * Math.random()) / 4);
+    }
+
+    return {
+      random_buf: random_buf,
+      partially_ordered_buf: partially_ordered_buf,
+      reversed_buf: reversed_buf,
+      few_unique_buf: few_unique_buf
+    };
+  }
+
+  componentDidMount () {
+    this.reset();
+  }
+
+  reset () {
+    this.setState(this.getState());
   }
 
   render () {
-    var buf = [];
-    for (var i = 0; i < 20; i++) {
-      buf.push(250 * Math.random());
-    }
+
     return (
       <div>
-        <Sorter type="selection-sort" list={buf}/>
-        <Sorter type="insertion-sort" list={buf}/>
-        <Sorter type="bubble-sort"    list={buf}/>
+        <div className="row">
+          <h1></h1>
+          <h1>Selection Sort</h1>
+          <h1>Insertion Sort</h1>
+          <h1>Bubble Sort</h1>
+        </div>
+        <div className="row">
+          <h2>Random</h2>
+          <Sorter type="selection-sort" list={this.state.random_buf}/>
+          <Sorter type="insertion-sort" list={this.state.random_buf}/>
+          <Sorter type="bubble-sort"    list={this.state.random_buf}/>
+        </div>
+        <div className="row">
+          <h2>Nearly Sorted</h2>
+          <Sorter type="selection-sort" list={this.state.partially_ordered_buf}/>
+          <Sorter type="insertion-sort" list={this.state.partially_ordered_buf}/>
+          <Sorter type="bubble-sort"    list={this.state.partially_ordered_buf}/>
+        </div>
+        <div className="row">
+          <h2>Reversed</h2>
+          <Sorter type="selection-sort" list={this.state.reversed_buf}/>
+          <Sorter type="insertion-sort" list={this.state.reversed_buf}/>
+          <Sorter type="bubble-sort"    list={this.state.reversed_buf}/>
+        </div>
+        <div className="row">
+          <h2>Few Unique</h2>
+          <Sorter type="selection-sort" list={this.state.few_unique_buf}/>
+          <Sorter type="insertion-sort" list={this.state.few_unique_buf}/>
+          <Sorter type="bubble-sort"    list={this.state.few_unique_buf}/>
+        </div>
       </div>
     );
   }
