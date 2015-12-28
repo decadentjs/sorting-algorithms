2527c59da8e11f21389780b3245378a50aa3e2ba
claudiopro <claudio.procida@gmail.com>
2015-12-28 13:04:25 +0100

    Begins resumifying heap sort

--- src/heap_sort.js
+++ src/heap_sort.js
@@ -12,21 +12,30 @@ function iRightChild(i) {
   return 2*i + 2;
 }
 
-function heapify(a) {
-  var start = iParent(a.length - 1);
+function heapify(a, resume) {
+  var start = resume && resume.i || iParent(a.length - 1);
 
   while (start >= 0) {
-    siftDown(a, start, a.length - 1);
+    siftDown(a, start, a.length - 1, resume);
     start--;
+    return {
+      i: start,
+      finished: false
+    };
   }
+
+  return {
+    finished: false,
+    heapified: true
+  };
 }
 
 function foo() {
   var bar =1 ;
 }
 
-function siftDown(a, start, end) {
-  var root = start, child, swp;
+function siftDown(a, start, end, resume) {
+  var root = resume && resume.i || start, child, swp;
   while (iLeftChild(root) <= end) {
     child = iLeftChild(root);
     swp = root;
@@ -37,17 +46,26 @@ function siftDown(a, start, end) {
       swp = child + 1;
     }
     if (swp === root) {
-      return;
+      return {
+        finished: false,
+        sifted: true
+      };
     }
     else {
       swap(a, root, swp);
       root = swp;
     }
+    return {
+      finished: false,
+      i: root
+    };
   }
 }
 
 function heap_sort(a, resume) {
-  heapify(a);
+  if (!(resume && resume.heapified)) {
+    return heapify(a, resume);
+  }
 
   var end = resume && resume.end || a.length - 1;
 
